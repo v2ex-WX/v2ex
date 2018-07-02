609a568bdcba60a655268ae592fc4388e2696af2
fjs <fjs@fjsdeMac-mini.local>
2018-07-02 18:07:31 +0800

    一级页面添加模板

---
+++ src/pages/more/index.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class='container'>
+  
+    </div>
+</template> 
+
+<script>
+export default {
+  data() {
+    return {
+  
+    };
+  },
+   components: {
+    
+  },
+  methods: {
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.container {
+  background-color: #efefef;
+  font-size: 30rpx;
+  overflow: hidden;
+  width: 100%;
+  position: relative;
+  padding: 0rpx;
+  
+}
+</style>
